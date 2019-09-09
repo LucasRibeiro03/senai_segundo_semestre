df6b05f322ee7628b9d604443cdc63ef5c3bfe16
Lucas Ribeiro Da Silva <45848873810@info.local>
2019-09-09 17:26:24 -0300

    up

---
+++ OpFlix/Senai.OpFlix.WebApi/Senai.OpFlix.WebApi/Contexts/AutoPecasContext.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.OpFlix.WebApi.Domains
+{
+    public partial class AutoPecasContext : DbContext
+    {
+        public AutoPecasContext()
+        {
+        }
+
+        public AutoPecasContext(DbContextOptions<AutoPecasContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Fornecedores> Fornecedores { get; set; }
+        public virtual DbSet<Pecas> Pecas { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=.\\SqlExpress; Initial Catalog=T_AutoPecas;User Id=sa;Pwd=132
+");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedores>(entity =>
+            {
+                entity.HasKey(e => e.IdFornecedor);
+
+                entity.HasIndex(e => e.Cnpj)
+                    .HasName("UQ__Forneced__AA57D6B4E629DF62")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.IdUsuario)
+                    .HasName("UQ__Forneced__5B65BF96F290AE19")
+                    .IsUnique();
+
+                entity.HasIndex(e => e.RazaoSocial)
+                    .HasName("UQ__Forneced__448779F058A40E65")
+                    .IsUnique();
+
+                entity.Property(e => e.Cnpj)
+                    .IsRequired()
+                    .HasColumnName("CNPJ")
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Endereco)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeFornecedor)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.RazaoSocial)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithOne(p => p.Fornecedores)
+                    .HasForeignKey<Fornecedores>(d => d.IdUsuario)
+                    .HasConstraintName("FK__Fornecedo__IdUsu__4F7CD00D");
+            });
+
+            modelBuilder.Entity<Pecas>(entity =>
+            {
+                entity.HasKey(e => e.IdPecas);
+
+                entity.HasIndex(e => e.CodigoPeca)
+                    .HasName("UQ__Pecas__B9F4770FAD127A5E")
+                    .IsUnique();
+
+                entity.Property(e => e.CodigoPeca)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Descricao)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Peso)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrecoCusto)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrecoVenda)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.FornecedorVinculadoNavigation)
+                    .WithMany(p => p.Pecas)
+                    .HasForeignKey(d => d.FornecedorVinculado)
+                    .HasConstraintName("FK__Pecas__Fornecedo__534D60F1");
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("UQ__Usuarios__A9D105340DC8D5D7")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(300)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
